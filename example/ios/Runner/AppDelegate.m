a704679f3d6b450565722508848e9de21105ffe8
Guillaume Bernos <guillaume.bernos@devoteamgcloud.com>
2019-04-20 20:08:25 +0200

    register headless plugin new app

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -2,11 +2,19 @@
 #include "GeneratedPluginRegistrant.h"
 #import "GoogleMaps/GoogleMaps.h"
 
+#import <location/LocationPlugin.h>
+
 @implementation AppDelegate
+
+void registerPlugins(NSObject<FlutterPluginRegistry>* registry) {
+  [GeneratedPluginRegistrant registerWithRegistry:registry];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GMSServices provideAPIKey:@"YOUR API KEY"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [LocationPlugin setPluginRegistrantCallback:registerPlugins];
+
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
